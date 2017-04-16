3d0945f87dcbbb0e299f2f60180e387060179837
James White <j_n_white@hotmail.com>
2017-04-16 11:10:46 +0100

    Mock table

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Hello world!</h2>
+        <h2>Games to be Played</h2>
+        <table>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Date started</th>
+          <tr>
+            <td>The Longest Journey</td>
+            <td>Game</td>
+            <td>09/04/17</td>
+          </tr>
+          <tr>
+            <td>Dreamfall</td>
+            <td>Game</td>
+            <td></td>
+          </tr>
+        </table>
       </div>
     );
   }
